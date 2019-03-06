4174a6aa7b7027bf90371a41504e7429127898ba
ComplYue <compl.yue@icloud.com>
2019-03-06 16:17:24 +0800

    ping in a dedicated goro, improve alive checking logic

--- pkg/ccm/pulse.go
+++ pkg/ccm/pulse.go
@@ -14,13 +14,16 @@ import (
 
 type PulseCfg struct {
 	// user name used in ssh url
-	SshUser string
+	SshUser string `yaml:"sshUser"`
 
 	// don't repeat check too often
-	CheckInterval time.Duration
+	CheckInterval time.Duration `yaml:"checkInterval"`
 
 	// confirm death only after this long
-	DeathConfirm time.Duration
+	DeathConfirm time.Duration `yaml:"deathConfirm"`
+
+	// forget after this long
+	ForgetDead time.Duration `yaml:"forgetDead"`
 }
 
 var pulseCfg *PulseCfg
@@ -32,34 +35,11 @@ func GetPulseCfg() *PulseCfg {
 		if err != nil {
 			panic(err)
 		}
-		var cfgYaml yaml.MapSlice
+		var cfgYaml PulseCfg
 		if err = yaml.Unmarshal(cfgRawYaml, &cfgYaml); err != nil {
 			panic(err)
 		}
-		sshUser, checkInterval, deathConfirm := "root", "1h", "48h"
-		for _, cfgItem := range cfgYaml {
-			if cfgKey, ok := cfgItem.Key.(string); ok {
-				if "sshUser" == cfgKey {
-					sshUser = cfgItem.Value.(string)
-				} else if "checkInterval" == cfgKey {
-					checkInterval = cfgItem.Value.(string)
-				} else if "deathConfirm" == cfgKey {
-					deathConfirm = cfgItem.Value.(string)
-				}
-			}
-		}
-		CheckInterval, err := time.ParseDuration(checkInterval)
-		if err != nil {
-			panic(err)
-		}
-		DeathConfirm, err := time.ParseDuration(deathConfirm)
-		if err != nil {
-			panic(err)
-		}
-		pulseCfg = &PulseCfg{
-			SshUser:       sshUser,
-			CheckInterval: CheckInterval, DeathConfirm: DeathConfirm,
-		}
+		pulseCfg = &cfgYaml
 	} else {
 		// todo reload on cfg file modified
 	}
@@ -78,6 +58,7 @@ type IpAliveness struct {
 var (
 	aliveness       = make(map[string]IpAliveness)
 	alivenessMutext sync.Mutex
+	aliveCheckQueue = make(chan IpAliveness, 500)
 )
 
 func CareIpAliveness(ip string, AssumeAlive bool, cfg interface{}) {
@@ -96,72 +77,109 @@ func CareIpAliveness(ip string, AssumeAlive bool, cfg interface{}) {
 	}
 }
 
-func CheckIpAlive(ip string) (bool, time.Time, interface{}) {
-	alivenessMutext.Lock()
-	defer alivenessMutext.Unlock()
+func init() {
+	go func() {
 
-	return _checkIpAlive(ip)
-}
+		for {
+			a2c := <-aliveCheckQueue
 
-func _checkIpAlive(ip string) (bool, time.Time, interface{}) {
+			pulseCfg := GetPulseCfg()
+			now := time.Now()
+			if now.Before(a2c.LastCheck.Add(pulseCfg.CheckInterval)) {
+				// not repeating check within interval
+				continue
+			}
 
-	pulseCfg := GetPulseCfg()
-	knownState, caring := aliveness[ip]
+			glog.V(1).Infof("Pinging ip=[%s] for alive check ...", a2c.IP)
+			pingCmd := exec.Command("ping", "-c", "3", a2c.IP)
+			pingCmd.Stdin, pingCmd.Stdout, pingCmd.Stderr = nil, nil, nil
+			if err := pingCmd.Run(); err == nil {
+				glog.V(1).Infof("IP [%s] is alive.", a2c.IP)
+				// start/continue caring its aliveness as got positive result at this instant
+				a2c.CheckedAlive, a2c.LastCheck = true, now
+				a2c.AssumeAlive, a2c.LastAlive = true, now
+			} else if ee, ok := err.(*exec.ExitError); ok {
+				glog.V(1).Infof("IP [%s] not alive, ping %+v", a2c.IP, ee)
+				a2c.CheckedAlive, a2c.LastCheck = false, now
+				if a2c.AssumeAlive { // check if death can be confirmed now
+					if now.After(a2c.LastAlive.Add(pulseCfg.DeathConfirm)) {
+						// confirm death after the configured duration
+						a2c.AssumeAlive = false
+					} else {
+						// not positive alive, but keep assumption for now
+					}
+				}
+				if !a2c.AssumeAlive && now.After(a2c.LastAlive.Add(pulseCfg.ForgetDead)) {
+					// forget about this IP
+					func() {
+						alivenessMutext.Lock()
+						defer alivenessMutext.Unlock()
 
-	if caring && knownState.AssumeAlive {
-		// assuming alive
-		if time.Now().Before(knownState.LastAlive.Add(pulseCfg.CheckInterval)) {
-			// assumption still valid
-			return true, knownState.LastAlive, knownState.Cfg
+						delete(aliveness, a2c.IP)
+					}()
+				}
+			} else {
+				glog.Errorf("Unexpected error calling ping: %+v", err)
+				continue
+			}
+
+			func() {
+				alivenessMutext.Lock()
+				defer alivenessMutext.Unlock()
+
+				aliveness[a2c.IP] = a2c
+			}()
 		}
 
-		// not actually alive after the configured interval, check should be carried out periodically
+	}()
+}
 
-		if !knownState.LastCheck.IsZero() && time.Now().Before(knownState.LastCheck.Add(pulseCfg.CheckInterval)) {
-			// no hurry to repeat another death check
-			return true, knownState.LastAlive, knownState.Cfg
+func CheckIpAlive(ip string) (bool, time.Time, interface{}) {
+	pulseCfg := GetPulseCfg()
+	knownState, caring := aliveness[ip]
+	now := time.Now()
+
+	if caring { // check should be carried out periodically
+		if !now.Before(knownState.LastAlive.Add(pulseCfg.CheckInterval)) {
+			aliveCheckQueue <- knownState
 		}
 	}
 
-	now := time.Now()
+	if caring && knownState.AssumeAlive { // still assuming alive
+		return true, knownState.LastAlive, knownState.Cfg
+	}
+
 	pingCmd := exec.Command("ping", "-c", "3", ip)
 	pingCmd.Stdin, pingCmd.Stdout, pingCmd.Stderr = nil, nil, nil
 	if err := pingCmd.Run(); err == nil {
 		glog.V(1).Infof("IP [%s] is alive.", ip)
-		// start/continue caring its aliveness as got positive result at this instant
+		// got positive result at this instant
+		// start/continue caring its aliveness as
 		knownState = IpAliveness{
 			IP:          ip,
 			AssumeAlive: true, LastAlive: now,
 			CheckedAlive: true, LastCheck: now,
 			Cfg: knownState.Cfg,
 		}
-		aliveness[ip] = knownState
 	} else if ee, ok := err.(*exec.ExitError); ok {
 		glog.V(1).Infof("IP [%s] not alive, ping %+v", ip, ee)
 		if caring {
-			knownState.CheckedAlive = false
-			knownState.LastCheck = now
-			if knownState.AssumeAlive { // check if death can be confirmed now
-				if now.After(knownState.LastAlive.Add(pulseCfg.DeathConfirm)) {
-					// confirm death after the configured duration
-					knownState = IpAliveness{
-						IP:          ip,
-						AssumeAlive: false, LastAlive: knownState.LastAlive,
-						CheckedAlive: false, LastCheck: now,
-						Cfg: nil,
-					}
-					aliveness[ip] = knownState
-				} else {
-					// not positive alive, but keep assumption for now
-					return true, knownState.LastAlive, knownState.Cfg
-				}
-			}
-			// not assuming alive, return affirmative dead conclusion
+			knownState.CheckedAlive, knownState.LastCheck = false, now
+			// the dedicated checker goro will confirm its death
+		} else {
+			// not on record, available to be assigned
 			return false, time.Time{}, nil
 		}
 	} else {
 		panic(errors.Errorf("Unexpected error calling ping: %+v", err))
 	}
+
+	func() {
+		alivenessMutext.Lock()
+		defer alivenessMutext.Unlock()
+
+		aliveness[ip] = knownState
+	}()
 
 	return knownState.AssumeAlive, knownState.LastAlive, knownState.Cfg
 }
@@ -176,13 +194,10 @@ func ListCaredIPs() []IpAliveness {
 		defer alivenessMutext.Unlock()
 
 		checkThres := time.Now().Add(-pulseCfg.CheckInterval)
-		for ip, ca := range aliveness {
-			if ca.LastCheck.Before(checkThres) {
-				if alive, lastAlive, cfg := _checkIpAlive(ip); alive && cfg != ca.Cfg {
-					glog.Warningf("ip=[%s] alive up to %v as another cfg:\n%+v\n - vs -\n%+v",
-						ip, lastAlive, cfg, ca.Cfg)
-					continue
-				}
+		for _, ca := range aliveness {
+			if !ca.LastCheck.After(checkThres) {
+				// schedule a new check for it
+				aliveCheckQueue <- ca
 			}
 			caList = append(caList, ca)
 		}
